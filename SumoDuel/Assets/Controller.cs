84cf219f808c168bfc5819307d5e45f4c97f5170
Anton Konovalov <konovalov.anton.39@gmail.com>
2017-05-29 23:42:47 +0300

    [create] animations + simple player controller

---
+++ SumoDuel/Assets/Controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    [SerializeField]    private float _rotationSpeed;
+    [SerializeField]    private float _movementSpeed;
+    private Vector3 _rotation;
+
+	void Start () {
+		
+	}
+
+
+    void Update () {
+
+        float x = Input.GetKey(KeyCode.A) ? -1 : Input.GetKey(KeyCode.D) ? 1 : 0;
+        float y = Input.GetKey(KeyCode.S) ? -1 : Input.GetKey(KeyCode.W) ? 1 : 0;
+
+        if (Mathf.Abs(x) > 0 || Mathf.Abs(y) > 0)
+        {
+            FixRotation(x,y);
+            MoveForward();
+        }   
+	}
+
+    private void MoveForward()
+    {
+        transform.position = transform.position + transform.up * Time.deltaTime * _movementSpeed; 
+    }
+
+    private void FixRotation(float x, float y)
+    {
+        _rotation.x = x;
+        _rotation.y = y;
+        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _rotation);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * _rotationSpeed);
+    }
+}
